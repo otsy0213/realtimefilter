1047a973c9a3de6c0a139f782822dfe1ecc3cbc6
otsy0213 <78540802+otsy0213@users.noreply.github.com>
2021-03-22 21:19:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(function () {
     searchWord = function(){
-      var searchText = $(this).val(), // 検索ボックスに入力された値
+      let searchText = $(this).val(), // 検索ボックスに入力された値
       targetText;
   
-      $('.target-area li').each(function() {
+      $('.target-area div').each(function() {
         targetText = $(this).text();
   
         // 検索対象となるリストに入力された文字列が存在するかどうかを判断
